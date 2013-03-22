0d037006c23863e5d34dc4fa2be752865e60c4f8
Altech <takeno.sh@gmail.com>
2013-03-23 06:07:17 +0900

    third commit.
    - add-note command
    - sort files
    - draw underline of table

--- ever-mode.el
+++ ever-mode.el
@@ -2,7 +2,8 @@
   (define-key ever-mode-map (kbd "n") 'ever-goto-next-note)
   (define-key ever-mode-map (kbd "p") 'ever-goto-previous-note)
   (define-key ever-mode-map (kbd "s") 'ever-search-notes)
-  (define-key ever-mode-map (kbd "q") 'ever-quit))
+  (define-key ever-mode-map (kbd "q") 'ever-quit)
+  (define-key ever-mode-map (kbd "a") 'ever-add-note))
 
 (defun ever-buffer-switch (buf)
   (if (string-match "ever" (buffer-name))
@@ -17,13 +18,18 @@
     (hl-line-mode))
   (setq buffer-read-only nil)
   (let* ((files (ever-notes-filter (directory-files ever-root-directroy t))) (lists (mapcar 'ever-file-to-list files)) (column-width (ever-get-column-width lists)))
-    files ; => ("/Users/Altech/Notes/oauth.md" "/Users/Altech/Notes/scala.org" "/Users/Altech/Notes/test.md" "/Users/Altech/Notes/test.txt" "/Users/Altech/Notes/オレオレEvernote.org")
-    lists ; => (("2013-03-22" "md" "oauth") ("2013-03-22" "org" "scala") ("2013-03-19" "md" "test") ("2013-03-18" "txt" "test") ("2013-03-19" "org" "オレオレEvernote"))
+    files ; => ("/Users/Altech/Notes/Emacsメモツール.org" "/Users/Altech/Notes/Hive勉強会資料.md" "/Users/Altech/Notes/OAuthの仕組み.md" "/Users/Altech/Notes/Scalaまとめ.org" "/Users/Altech/Notes/test.md" "/Users/Altech/Notes/test.txt" "/Users/Altech/Notes/コンピュータプログラミングの概念・技法・モデル.org" "/Users/Altech/Notes/パターン認識の応用例.md" "/Users/Altech/Notes/ファイルシステム実装.md" "/Users/Altech/Notes/数理計画法.md" "/Users/Altech/Notes/旅行調査.md" "/Users/Altech/Notes/読書メーター代替.md")
+    lists ; => (("2013-03-19" "org" "Emacsメモツール") ("2013-02-09" "md" "Hive勉強会資料") ("2013-03-22" "md" "OAuthの仕組み") ("2013-03-22" "org" "Scalaまとめ") ("2013-03-22" "md" "test") ("2013-03-18" "txt" "test") ("2012-05-15" "org" "コンピュータプログラミングの概念・技法・モデル") ("2012-10-12" "md" "パターン認識の応用例") ("2013-02-14" "md" "ファイルシステム実装") ("2012-10-16" "md" "数理計画法") ("2013-01-30" "md" "旅行調査") ("2012-12-24" "md" "読書メーター代替"))
+    (setq lists (sort lists (lambda (l1 l2)
+			     (nth 0 l1) ; => "2013-03-22", "2013-03-22", "2013-02-09", "2013-03-18", "2013-03-22", "2013-03-18", "2013-03-22", "2013-03-22", "2013-03-18", "2013-03-18", "2013-03-18", "2013-02-14", "2013-02-14", "2012-10-12", "2012-12-24", "2013-01-30", "2012-12-24", "2013-01-30", "2013-01-30", "2012-12-24", "2012-10-16", "2013-02-14", "2013-02-14", "2013-02-14", "2013-02-14", "2013-02-14", "2013-02-14", "2013-01-30"
+			     (nth 0 l2) ; => "2013-02-09", "2013-03-19", "2013-03-19", "2013-03-22", "2013-03-22", "2013-03-22", "2013-03-22", "2013-03-22", "2013-03-22", "2013-03-19", "2013-02-09", "2012-10-12", "2012-05-15", "2012-05-15", "2013-01-30", "2012-10-16", "2012-10-16", "2013-02-14", "2012-10-12", "2012-10-12", "2012-10-12", "2013-03-22", "2013-03-22", "2013-03-22", "2013-03-19", "2013-03-18", "2013-02-09", "2013-02-09"
+			     (not (string< (nth 0 l1) (nth 0 l2)))
+			     )))
     (setq lists (cons '("Updated-at" "Ext" "Title") lists))
     (erase-buffer)
-    (insert "\n [n]: next-note [p]: previous-note [s]: search [q]: quit \n\n")
+    (insert "\n [n]: next-note [p]: previous-note [a]: add-note [s]: search [q]: quit \n\n")
     (point) ; => 61
-    (insert (mapconcat (lambda (ls) (join ls "|")) (ever-make-table lists column-width) "\n")) ; => nil
+    (insert (ever-set-faces-to-table (mapconcat (lambda (ls) (join ls "|")) (ever-make-table lists column-width) "\n"))) ; => nil
     (insert "\n")
   )
   (setq buffer-read-only t)
@@ -32,6 +38,7 @@
   (beginning-of-buffer)
   (goto-line 6)
   (ever-goto-previous-note)
+  ;(put-text-property 1 10 'box (color-values "gray"))
   )
 
 
@@ -63,7 +70,6 @@
 
 (defun ever-search-notes (regexp)
   (interactive "sSearch: ")
-  regexp ; => 
   (rgrep regexp "*" ever-root-directroy nil)
   )
 
@@ -74,6 +80,11 @@
     (when (get-buffer (file-name-nondirectory path))
       (kill-buffer (file-name-nondirectory path)))
     ))
+
+(defun ever-add-note (title ext)
+  (interactive "sTitle: \nsExtension: ")
+  (with-current-buffer (pop-to-buffer nil)
+    (find-file (expand-file-name (concat title "." ext) ever-root-directroy))))
 
 (defun ever-notes-filter (list)
   (filter (lambda (s) (not (string-match "^\\..*" (file-name-nondirectory s)))) list)
@@ -97,6 +108,12 @@
 		      (ever-recenter-string (car cell) (cdr cell)))
 		    (zip list column-width)))
 	  lists))
+
+(defun ever-set-faces-to-table (lines)
+  lines ; => " Updated-at | Ext | Title                              \n 2013-03-22 | md  | test                                \n 2013-03-22 | org | Scalaまとめ                          \n 2013-03-22 | md  | OAuthの仕組み                        \n 2013-03-19 | org | Emacsメモツール                       \n 2013-03-18 | txt | test                                \n 2013-02-14 | md  | ファイルシステム実装                    \n 2013-02-09 | md  | Hive勉強会資料                        \n 2013-01-30 | md  | 旅行調査                              \n 2012-12-24 | md  | 読書メーター代替                        \n 2012-10-16 | md  | 数理計画法                            \n 2012-10-12 | md  | パターン認識の応用例                    \n 2012-05-15 | org | コンピュータプログラミングの概念・技法・モデル "
+  (let ((list (split-string lines "\n")))
+    (join (cons (propertize (concat (car list) "  ") 'face 'underline) (cdr list)) "\n"))
+  )
 
 ;; general-purpose routines
 
@@ -149,8 +166,9 @@
 
 
 ;; [TODO]
-;; - sort note
 ;; - spotlight search
 ;; - improve search result 
 ;; - tag
 ;; - improve face
+;; - mark-and-delete
+;; - reflesh file list
